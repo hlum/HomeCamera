422ae9c25b5cb5a0c206144e4e3ec07e753b90f7
aung <hlumaungphyo@gmail.com>
2025-06-24 20:46:36 +0900

    Initial Commit

---
+++ HomeCamera/HomeCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCameraApp.swift
+//  HomeCamera
+//
+//  Created by cmStudent on 2025/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
